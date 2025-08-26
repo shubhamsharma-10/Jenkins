f724e1d424e5fc597bf402f4ffb6c34308fbbbbe
Shubham Sharma <shubhamsharma10403@gmail.com>
2025-08-26 21:48:38 +0530

    project init

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import router from './routes/todos';
+const app = express();
+
+app.use(express.json());
+app.use('/api', router);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
